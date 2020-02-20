2c3ecfc0339fb56f63b2e1933e1c7a7b159fdd89
Cameron Pappas <cameron.pappas1@gmail.com>
2020-02-20 17:11:21 +1100

    Basic implicit flow detection

--- commons/src/main/scala/org/bitbucket/inkytonik/cooma/SemanticAnalyser.scala
+++ commons/src/main/scala/org/bitbucket/inkytonik/cooma/SemanticAnalyser.scala
@@ -191,33 +191,61 @@ class SemanticAnalyser(
             })
     }
 
-    // TODO: need to check function usage via Sel
-    def checkSecretCaseFunction(e : Expression) : Messages =
-        tipe(e) match {
-            case Some(FunT(ArgumentTypes(as), _)) => {
-                System.out.println("here")
-                as.flatMap(a => a.expression match {
-                    case SecT(_) => noMessages
-                    case _ =>
-                        error(a, s"function used in match on secret value cannot have public fields")
-                })
-            }
-            case _ => noMessages
-        }
-
-    // TODO: expand to check all blocks
-    def checkSecretCases(cs : Vector[Case]) : Messages =
-        cs.flatMap(c => tipe(c.expression) match {
-            case Some(Blk(b)) => b match {
-                case Return(e) => checkSecretCaseFunction(e)
-                case _         => noMessages
-            }
-            case _ => noMessages
+    def checkSecretArgumentTypes(as : Vector[ArgumentType]) : Messages =
+        as.flatMap(a => a.expression match {
+            case SecT(_) => noMessages
+            case _ =>
+                error(a.expression, s"function with public field used from secret case")
         })
 
+    // Function is called when a match on case statements occurs
+    // Should recursively check the each expression contains only function
+    // We only care about out of scope function usage either from a record field or idn use
+    def checkSecretExpression(e : Expression) : Messages = {
+        e match {
+            case App(exp, _) => checkSecretExpression(exp) // Functions should occur on left?
+            case Blk(b) => b match {
+                case BlkDef(_, exp) => checkSecretExpression(Blk(exp)) // FIXME Nasty solution
+                case BlkLet(Let(_, _, _, exp), _) =>
+                    checkSecretExpression(exp)
+                case Return(exp) => checkSecretExpression(exp)
+            }
+            case i @ Idn(IdnUse(x)) =>
+                (tipe(i) match {
+                    case Some(FunT(ArgumentTypes(as), _)) =>
+                        checkSecretArgumentTypes(as)
+                    case _ =>
+                        noMessages
+                }) ++ (lookup(env(i), x, UnknownEntity()) match {
+                    case FunctionEntity(Def(_, Body(_, _, b))) =>
+                        checkSecretExpression(b)
+                    case _ => noMessages
+                })
+            case s @ Sel(i @ Idn(IdnUse(x)), FieldUse(field)) => { // Functions that are part of records?
+                // System.out.println(lookup(env(i), x, UnknownEntity()))
+                (lookup(env(i), x, UnknownEntity())) match {
+                    case ArgumentEntity(Argument(_, exp, _)) => {
+                        System.out.println(unalias(s, exp))
+                        unalias(s, exp) match {
+                            case Some(RecT(fs)) => {
+                                System.out.println(unalias)
+                            }
+                        }
+                    }
+                    case _ =>
+                        noMessages
+                }
+                checkSecretExpression(i)
+            }
+            case _ =>
+                noMessages
+        }
+    }
+
+    // If secret ensure secret context for each case statment
     def checkSecretMatch(e : Expression, cs : Vector[Case]) : Messages =
         tipe(e) match {
-            case Some(SecT(_)) => checkSecretCases(cs)
+            case Some(SecT(_)) => cs.flatMap(c => checkSecretExpression(c.expression))
             case _             => noMessages
         }
 
@@ -679,8 +707,9 @@ class SemanticAnalyser(
                 else
                     tipe(m.expression) match {
                         case Some(SecT(_)) => caseTypes(0) match { // Enforce return type is Sec
-                            case Some(t) => OkCases(Some(SecT(t)))
-                            case None    => OkCases(None) // Unreachable??
+                            case s @ Some(SecT(_)) => OkCases(s) // Do nothing if already sec type
+                            case Some(t)           => OkCases(Some(SecT(t)))
+                            case None              => OkCases(None) // Unreachable??
                         }
                         case _ => OkCases(caseTypes(0))
                     }
