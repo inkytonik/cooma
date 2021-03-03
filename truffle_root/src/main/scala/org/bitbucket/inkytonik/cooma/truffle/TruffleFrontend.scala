4dfb5742def6a50629a5f93804793e99bde03bfb
Tony Sloane <tony.sloane@me.com>
2021-03-03 11:53:28 +1100

    Minor formatting tidy-up

--- truffle_root/src/main/scala/org/bitbucket/inkytonik/cooma/truffle/TruffleFrontend.scala
+++ truffle_root/src/main/scala/org/bitbucket/inkytonik/cooma/truffle/TruffleFrontend.scala
@@ -13,19 +13,18 @@ class TruffleFrontend(in : InputStream = System.in, out : PrintStream = System.o
      * Main entry point, where a cooma file is provided to run in the config.
      * @param config
      */
-    override def interpret(config : Config) : Unit = {
+    override def interpret(config : Config) : Unit =
         if (config.filenames().isEmpty) {
             val repl = new TruffleDriver().createREPL(config)
             repl.driver(config.args.toIndexedSeq)
         } else {
             interpret("", config)
         }
-    }
 
     override def interpret(programName : String, program : String, config : Config) : Unit =
         interpret(program, config)
 
-    private def interpret(program : String, config : Config) : Unit = {
+    private def interpret(program : String, config : Config) : Unit =
         try {
             val context = createContext(config)
             val result = context.eval(CoomaConstants.ID, program)
@@ -34,7 +33,6 @@ class TruffleFrontend(in : InputStream = System.in, out : PrintStream = System.o
             case e : PolyglotException =>
                 config.output().emit(e.getMessage)
         }
-    }
 
     private def printAndClose(config : Config, context : Context, result : Value) = {
         if (CoomaLanguage.Type.Error.value.equals(result.getMetaObject.toString))
@@ -48,4 +46,5 @@ class TruffleFrontend(in : InputStream = System.in, out : PrintStream = System.o
         Context.newBuilder(CoomaConstants.ID)
             .out(out).in(in).arguments(CoomaConstants.ID, config.args.toArray)
             .build()
+
 }
